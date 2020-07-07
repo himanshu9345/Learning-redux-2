71848ff8c821fe298c181f59fea11f85338c3f48
himanshu9345 <hbp53@njit.edu>
2020-07-06 20:52:26 -0400

    displayed users(contacts) to  sidebar

--- skypey/src/App.js
+++ skypey/src/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import Sidebar from "./Sidebar";
 import Main from "./Main";
 import "./App.css";
+import store from "./store";
+import _ from "lodash";
+
 const App = () => {
+  const { contacts } = store.getState();
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar contacts={_.values(contacts)} />
       <Main />
     </div>
   );
