fdc0168aadef6458d7d7f6448323ade83745f712
himanshu9345 <hbp53@njit.edu>
2020-07-07 23:19:32 -0400

    completed chatting window

--- skypey/src/reducers/activeUserId.js
+++ skypey/src/reducers/activeUserId.js
@@ -1,5 +1,4 @@
 import { SET_ACTIVE_USER_ID } from "../actions/constants/action-types";
-import store from "../store";
 export default function activeUserId(state = null, action) {
   switch (action.type) {
     case SET_ACTIVE_USER_ID:
